1e5805a5fb6c13ea46ef88ac81c1e62d5a32c426
jowiealegre <jowiealegre@gmail.com>
2020-02-10 14:42:22 -0300

    ejercicio 1

---
+++ script.js
@@ -0,0 +1,8 @@
+/*  hacer una funcion que devuelva los numeros dentro de un array multiplicando cada numero por 2 */
+/*function multiplicar (listanum) {
+    let numx2=[];
+    for (let i=0; i < listanum.length; i++){
+        numx2.push (listanum[i] * 2);
+    }
+    return numx2;
+}*/
