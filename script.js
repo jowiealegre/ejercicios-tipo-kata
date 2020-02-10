d8e0828268660b4c57701f070afc457fe495b9ea
jowiealegre <jowiealegre@gmail.com>
2020-02-10 16:08:33 -0300

    ejercicio3

--- script.js
+++ script.js
@@ -8,9 +8,25 @@
 }*/
 
 /* hacer una funcion que devuelva la fcha actual en formato dd/mm/aa*/
-let fecha new Date ();
+
+/*let fecha new Date ();
 let dia = fecha.getDate ();
 let mes = fecha.getMonth ();
 let anio =fecha.getFullYear ();
-console.log (dia + '/' mes +'/' anio);
+console.log (dia + '/' mes +'/' anio); */
+
+/* hacer una funcion que permita saber  cuantos dias faltan para que acabe el año*/
+
+function diasdelaño (){
+    const meses =[31,28,31,30,31,30,31,31,30,31,30,31]
+    let restante =365;
+    let fecha = new Date ();
+    for (let i = 0;i < fecha.getMonth();i++){
+        restante=restante-120
+    }
+return restante - fecha.getDate();
+}
+
+
+
 
